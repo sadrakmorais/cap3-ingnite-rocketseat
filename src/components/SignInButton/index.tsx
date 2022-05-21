a6a163261be3edcd9c0b665539cfb08c9defb3c6
sadrakmorais <sadrak.morais@gcbinvestimentos.com>
2022-05-21 15:49:44 -0300

    chore: Integration with FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,25 +1,29 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
+import { signIn, signOut, useSession } from 'next-auth/react';
+
 export function SignInButton() {
 
-  const isLogged = true;
+  const { data: session } = useSession();
 
-  return isLogged ? (
+  return session ? (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
-      <FaGithub
-        color="#04B361"
-      />
-      Zadraki
+      <div className={styles.avatar}>
+        <img src={session.user.image} alt={`Imagem do usuário ${session.user.name}`} />
+      </div>
+      {session.user.name}
       <FiX color='#737380' className={styles.closeButton} />
     </button>
   ) : (
     <button
       type='button'
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub
         color="#EBA417"
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,50 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { query } from "faunadb";
+import { fauna } from "../../../services/fauna";
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    session({ session }) {
+      return session;
+    },
+
+    async signIn({ user }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(query.Collection("users"), { data: { email } }),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
